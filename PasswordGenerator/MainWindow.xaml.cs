b8c1130fc780f5e1d78b4961f6e229c22b8f9c1d
StijnHemelings <StijnHemelings@users.noreply.github.com>
2022-02-15 23:35:48 +0100

    Add project files.

---
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Random random = new Random();
+
+        //Button Minimize App
+        private void MinimizeApp_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        //Button Close App
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void StackPanel_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void GeneratePassword_Click(object sender, RoutedEventArgs e)
+        {
+            string allCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789$*#@%&";
+            string randomPassword = "";
+
+            for (int i = 0; i < PasswordLengthSlider.Value; i++)
+            {
+                int randomNumber = random.Next(0, allCharacters.Length);
+                randomPassword += allCharacters[randomNumber];
+            }
+
+            GeneratedPassword.Text = randomPassword;
+        }
+
+        private void CopyToClipboard_Click(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(GeneratedPassword.Text);
+        }
+    }
+}
